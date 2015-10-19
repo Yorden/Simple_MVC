7f8ed991a5e121e7388cac0377c2623016d91ffa
Yorden <jmk1637@rit.edu>
2015-10-18 21:46:13 -0400

    added dog model

---
+++ src/models/Dog.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+var CatModel;
+
+var DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    breed: {
+        type: String,
+        trim:true,
+        required: true
+    },
+    age:{
+        type:Number,
+        trim:true,
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+DogSchema.statics.findByName = function(name, callback) {
+
+    var search = {
+        name: name
+    };
+
+    return DogModel.findOne(search, callback);
+};
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
